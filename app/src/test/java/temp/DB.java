f6c65f20503ed89aa4ab1d8624620a4996cce6f3
saimoon-oman <alfarshi1313@gmail.com>
2022-05-11 00:54:42 +0600

    new file added

---
+++ app/src/test/java/temp/DB.java
@@ -0,0 +1,26 @@
+package temp;
+
+public class DB {
+    int a, b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public DB(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}

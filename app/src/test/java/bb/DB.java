7df65477a5308af7dd9583826c1f857f6d0da80b
saimoon-oman <alfarshi1313@gmail.com>
2022-05-11 01:03:07 +0600

    new file in branch

---
+++ app/src/test/java/bb/DB.java
@@ -0,0 +1,26 @@
+package bb;
+
+public class DB {
+    double a, b;
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public DB(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}

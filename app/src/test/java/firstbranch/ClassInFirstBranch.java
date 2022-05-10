87bf4be4e1b326d7c79b79303717718aaa37e8d4
saimoon-oman <alfarshi1313@gmail.com>
2022-05-10 16:59:03 +0600

    modified class from master

--- app/src/test/java/firstbranch/ClassInFirstBranch.java
+++ app/src/test/java/firstbranch/ClassInFirstBranch.java
@@ -3,10 +3,12 @@ package firstbranch;
 public class ClassInFirstBranch {
     String name;
     int roll;
+    double cgpa;
 
-    public ClassInFirstBranch(String name, int roll) {
+    public ClassInFirstBranch(String name, int roll, double cgpa) {
         this.name = name;
         this.roll = roll;
+        this.cgpa = cgpa;
     }
 
     public String getName() {
@@ -17,11 +19,19 @@ public class ClassInFirstBranch {
         this.name = name;
     }
 
+    public int getRoll() {
+        return roll;
+    }
+
     public void setRoll(int roll) {
         this.roll = roll;
     }
 
-    public int getRoll() {
-        return roll;
+    public double getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(double cgpa) {
+        this.cgpa = cgpa;
     }
 }
